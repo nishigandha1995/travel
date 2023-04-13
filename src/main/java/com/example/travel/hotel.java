cfa49283718837b185a85839101e3a8f57c97a76
nishi kutemate <nishik@gmail.com>
2023-04-13 16:53:58 +0530

    hello

---
+++ src/main/java/com/example/travel/hotel.java
@@ -0,0 +1,11 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hotel {
+    @GetMapping("/myhotel")
+    public String getData(){
+        return "Please book your hotel";
+    }
+
+}
